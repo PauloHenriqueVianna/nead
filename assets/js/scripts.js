ca2aa47d2d04e23ce395ef1a62540a1baab1773d
PauloHenriqueVianna <paulo@note-pronatec.(none)>
2013-12-05 16:36:10 -0200

    show all news for year

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 $('#container').fullContent({
     stages: 'div',
-    mapPosition: [{v: 2, h: 2}, {v: 1, h: 2}, {v: 3, h: 2 }, {v: 3, h: 1}, {v: 3, h: 3}, {v: 2, h: 1}, {v: 2, h: 3}, {v: 4, h: 2}, {v: 5, h: 2}],
+    mapPosition: [{v: 2, h: 2}, {v: 1, h: 2}, {v: 3, h: 2 }, {v: 3, h: 1}, {v: 3, h: 3}, {v: 2, h: 1}, {v: 2, h: 3}, {v: 4, h: 2}, {v: 5, h: 2}, {v: 6, h: 2}],
     stageStart: 1,
     idComplement: 'page_'
 });
@@ -15,6 +15,28 @@ $(function() {
   $(".noticia").click (function() {
   	var id = $(this).attr("id");
   	var div = $('#noticia-'+id);
+  	console.log(div.html());
+  	if (div.html() == ''){
+	  	$.ajax({  
+		  	type: "GET",  
+		  	url: "noticia",
+		  	data: "id=" + id,
+		  	success: function (response){
+		  		div.html(response);
+		  		div.slideToggle('slow');
+		  	}  
+		  }); 
+	}else{
+		div.slideToggle('slow');
+		}
+	return false; 
+  });  
+});
+
+$(function() {  
+  $(".todas-noticia").click (function() {
+  	var id = $(this).attr("id");
+  	var div = $('#todas-noticia-'+id);
   	console.log(div.html());
   	if (div.html() == ''){
 	  	$.ajax({  
--- nead/views.py
+++ nead/views.py
@@ -5,8 +5,10 @@ from django.template import RequestContext
 from noticias.models import Noticia
 
 def home(request):
-	noticias = Noticia.objects.values('id','titulo')
-	return render_to_response('index.html',RequestContext(request,{'noticias':noticias}))
+	noticias = Noticia.objects.values('id','titulo','criado_em').order_by('-criado_em')[:4]
+	todas_noticias = Noticia.objects.values('id','titulo','criado_em').order_by('-criado_em')
+	anos = todas_noticias.dates('criado_em', 'year',order='DESC')
+	return render_to_response('index.html',RequestContext(request,{'noticias':noticias, 'todas_noticias':todas_noticias, 'anos':anos}))
 
 def noticia(request):
 	id = request.GET.get('id')
