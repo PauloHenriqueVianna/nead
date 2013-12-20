15e05fa392dfa406e107fc321895d21134795158
PauloHenriqueVianna <paulo@note-pronatec.(none)>
2013-12-20 16:10:57 -0200

    add banner in initial page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -54,15 +54,3 @@ $(function() {
 	return false; 
   });  
 });
-
-$(function() {
-	$("a[rel=galeria]").fancybox({
-	
-					'transitionIn'		: 'none',
-					'transitionOut'		: 'none',
-					'titlePosition' 	: 'over',
-					'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
-						return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
-					}
-	});		
-});
